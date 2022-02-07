a7bd40e1072052d2703817feec5711347390fc68
wangxinwei18 <wangxinwei18@hotmail.com>
2022-02-07 18:14:18 +0800

    2nd commit

--- scripts/stop-app.sh
+++ scripts/stop-app.sh
@@ -1,9 +1,9 @@
 set -x
-PID=$(lsof -t -i:3000)
+PID=$(lsof -t -i:3001)
 if [ -z "${PID}" ]; then
     echo "no node app running..."
   else
-    echo "stop app running on port 3000 with PID ${PID}"
+    echo "stop app running on port 30001 with PID ${PID}"
     kill -9 ${PID}
 fi
 set +x
--- src/app.js
+++ src/app.js
@@ -68,6 +68,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running in port 3000...");
+app.listen(3001, () => {
+  console.log("Server is running in port 3001...");
 });
